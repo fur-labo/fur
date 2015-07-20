70100912a9a98254e7cf4c9423d83e7099fcdc51
Taka Okunishi <okunishinishi@gmail.com>
2015-07-20 13:24:09 +0900

    Add example banner

--- ci/render.sh
+++ ci/render.sh
@@ -7,3 +7,4 @@ DEST_DIR="${BASE_DIR}/docs/assets/fonts"
 cd ${BASE_DIR}
 
 npm run render -- "+(docs|lib|test)/**/.*.bud"
+node ci/render/render_examples.js
---
+++ ci/render/render_examples.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+/**
+ * @file Render examples.
+ */
+
+"use strict";
+
+var fur = require('../../lib'),
+    stringcase = require('stringcase'),
+    path = require('path'),
+    async = require('async');
+
+
+var basedir = path.resolve(__dirname, '../..');
+
+async.series([
+    function renderBannerImages(callback) {
+        var bannerExamples = require(basedir + '/docs/examples/.banner-example.json');
+        async.each(bannerExamples, function (config, callback) {
+            var filename = path.resolve(basedir, 'docs/examples/images/example-' + stringcase.spinalcase(config.text) + '-banner.svg');
+            fur.banner(filename, config, callback);
+        }, callback);
+    }
+], function (err) {
+    if (err) {
+        console.error(err);
+    }
+});
+
