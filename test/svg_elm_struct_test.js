8c5261797e52ae10fa626629a8d5529ff19f31d9
Taka Okunishi <okunishinishi@gmail.com>
2015-07-20 09:15:49 +0900

    Add image drawer

---
+++ test/svg_elm_struct_test.js
@@ -0,0 +1,12 @@
+/**
+ * Test case for svgElmStruct.
+ * Runs with nodeunit.
+ */
+
+var svgElmStruct = require('../lib/struct/svg_elm_struct.js');
+
+exports['Svg elm struct'] = function(test){
+
+    test.done();
+};
+
---
+++ test/svg_elm_test.js
@@ -0,0 +1,12 @@
+/**
+ * Test case for svgElm.
+ * Runs with nodeunit.
+ */
+
+var svgElm = require('../lib/drawing/elms/svg_elm.js');
+
+exports['Svg elm'] = function(test){
+
+    test.done();
+};
+
