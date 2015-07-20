e6d8ef6ac78f37c8c7dca1512eb8d2b343b799c9
Taka Okunishi <okunishinishi@gmail.com>
2015-07-21 07:00:19 +0900

    Add identicion methods

--- test/fur_test.js
+++ test/fur_test.js
@@ -9,10 +9,22 @@ var Fur = require('../lib/fur.js'),
 var tmpDir = path.resolve(__dirname, '../tmp');
 
 exports['Generate a banner.'] = function (test) {
-    new Fur().banner(tmpDir + '/fur-testing-logo.svg', {
+    new Fur().banner(tmpDir + '/fur-testing-banner.svg', {
         color: 'e',
         font: 'd',
         text: 'Bar'
+    }, function (err) {
+        test.ifError(err);
+        test.done();
+    });
+};
+
+
+exports['Generate a favicon.'] = function (test) {
+    new Fur().favicon(tmpDir + '/fur-testing-favicon.svg', {
+        color: 'k',
+        font: 'g',
+        text: 'E'
     }, function (err) {
         test.ifError(err);
         test.done();
